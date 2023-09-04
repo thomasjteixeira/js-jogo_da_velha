3647ff9a10aa786437f5e7fd0b1f96b46c0e63f1
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-09-04 15:32:44 -0300

    feat: função mudarJogador e setar as jogadas nos quadrados

--- jogo.js
+++ jogo.js
@@ -1,5 +1,31 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById("jogador-selecionado");
 
-function escolherQuadrado(id){
-  console.log(id);
+mudarJogador("X");
+
+function escolherQuadrado(id) {
+  // pegar o elemento clicado
+  var quadrado = document.getElementById(id);
+  
+  // se o quadrado já estiver preenchido, sai da função
+  if (quadrado.innerHTML !== "-") {
+    return;
+  }
+
+  quadrado.innerHTML = jogador;
+  quadrado.style.color = "#000";
+
+  // verifica qual é o jogador e muda para o outro
+  if (jogador === "X") {
+    jogador = "O";
+  } else {
+    jogador = "X";
+  }
+
+  mudarJogador(jogador);
+}
+
+function mudarJogador(valor) {
+  jogador = valor;
+  jogadorSelecionado.innerHTML = jogador;
 }
