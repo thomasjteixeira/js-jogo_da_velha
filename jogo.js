fa92934f2e11b763201e98aa7258269cc3576a9f
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-09-11 10:18:29 -0300

    :recycle: refactor: checaVencedor(), checaSequencia()

--- jogo.js
+++ jogo.js
@@ -10,16 +10,16 @@ var quadrados = document.getElementsByClassName("quadrado");
 mudarJogador("X");
 
 //função principal quando seleciona o quadrado
-function escolherQuadrado(id) {  
+function escolherQuadrado(id) {
   // se já tiver um vencedor, não pode marcar mais quadrados
   if (vencedor !== null) return;
-  
+
   // pegar o elemento clicado
   var quadrado = document.getElementById(id);
-  
+
   // se o quadrado já estiver preenchido, sai da função
   if (quadrado.innerHTML !== "-") return;
-  
+
   quantidadeJogadas++;
   quadrado.innerHTML = jogador;
   quadrado.style.color = "#000";
@@ -40,90 +40,51 @@ function mudarJogador(valor) {
 }
 
 function verificaEmpate() {
-  if (quantidadeJogadas === 9 && vencedor === null) {    
+  if (quantidadeJogadas === 9 && vencedor === null) {
     vencedorSelecionado.innerHTML = "Empate";
   }
 }
 
 // Verificar todos os quadrados para ver se tem um vencedor
 function checaVencedor() {
-  var quadrado1 = document.getElementById(1);
-  var quadrado2 = document.getElementById(2);
-  var quadrado3 = document.getElementById(3);
-  var quadrado4 = document.getElementById(4);
-  var quadrado5 = document.getElementById(5);
-  var quadrado6 = document.getElementById(6);
-  var quadrado7 = document.getElementById(7);
-  var quadrado8 = document.getElementById(8);
-  var quadrado9 = document.getElementById(9);
+  const combinacoesVencedoras = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
 
-  if (checaSequencia(quadrado1, quadrado2, quadrado3)) {
-    mudarCorQuadrado(quadrado1, quadrado2, quadrado3);
-    mudarVencedor(quadrado1);
-    return;
-  }
+  for (let combinacao of combinacoesVencedoras) {
+    //const [quadrado1, quadrado2, quadrado3] = combinacao.map(id => document.getElementById(id));
+    const quadrado1 = document.getElementById(combinacao[0]);
+    const quadrado2 = document.getElementById(combinacao[1]);
+    const quadrado3 = document.getElementById(combinacao[2]);
 
-  if (checaSequencia(quadrado4, quadrado5, quadrado6)) {
-    mudarCorQuadrado(quadrado4, quadrado5, quadrado6);
-    mudarVencedor(quadrado4);
-    return;
-  }
-
-  if (checaSequencia(quadrado7, quadrado8, quadrado9)) {
-    mudarCorQuadrado(quadrado7, quadrado8, quadrado9);
-    mudarVencedor(quadrado7);
-    return;
-  }
-
-  if (checaSequencia(quadrado1, quadrado4, quadrado7)) {
-    mudarCorQuadrado(quadrado1, quadrado4, quadrado7);
-    mudarVencedor(quadrado1);
-    return;
-  }
-
-  if (checaSequencia(quadrado2, quadrado5, quadrado8)) {
-    mudarCorQuadrado(quadrado2, quadrado5, quadrado8);
-    mudarVencedor(quadrado2);
-    return;
-  }
-
-  if (checaSequencia(quadrado3, quadrado6, quadrado9)) {
-    mudarCorQuadrado(quadrado3, quadrado6, quadrado9);
-    mudarVencedor(quadrado3);
-    return;
-  }
-
-  if (checaSequencia(quadrado1, quadrado5, quadrado9)) {
-    mudarCorQuadrado(quadrado1, quadrado5, quadrado9);
-    mudarVencedor(quadrado1);
-    return;
-  }
-
-  if (checaSequencia(quadrado3, quadrado5, quadrado7)) {
-    mudarCorQuadrado(quadrado3, quadrado5, quadrado7);
-    mudarVencedor(quadrado3);
-    return;
+    if (checaSequencia(quadrado1, quadrado2, quadrado3)) {
+      mudarCorQuadrado(quadrado1, quadrado2, quadrado3);
+      mudarVencedor(quadrado1);
+      return;
+    }
   }
 }
 
 function checaSequencia(quadrado1, quadrado2, quadrado3) {
-  var eigual = false;
-
-  if (
+  return (
     quadrado1.innerHTML !== "-" &&
     quadrado1.innerHTML === quadrado2.innerHTML &&
     quadrado1.innerHTML === quadrado3.innerHTML
-  ) {
-    eigual = true;
-  }
-
-  return eigual;
+  );
 }
 
 function mudarCorQuadrado(quadrado1, quadrado2, quadrado3) {
-  quadrado1.style.background = "#0f0";
-  quadrado2.style.background = "#0f0";
-  quadrado3.style.background = "#0f0";
+  quadrado1.style.background =
+    quadrado2.style.background =
+    quadrado3.style.background =
+      "#0f0";
 }
 
 function mudarVencedor(quadrado) {
